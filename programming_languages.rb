dd1c80c6175a5c5b2d1f7b676dcdb0a8387456f6
Ad Friedman <adien330@yahoo.com>
2019-03-19 22:05:50 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,51 +1,51 @@
 def reformat_languages(languages)
   ab = languages.keys
-  
+
   #languages[i][d]
-  
+
   ij = languages[ab[0]].to_a
-  
+
   i, c, b = 0, [], []
-      while i < ij.length 
+      while i < ij.length
             if !ij[i].include?(:javascript)
             c << ij[i]
-            else 
+            else
       b << ij[i]
-      end 
+      end
       i+=1
       end
   c
-  
-  
+
+
   jj = languages[ab[1]].to_a
-  
+
   k, t = [], 0
-  while t < jj.length 
+  while t < jj.length
     if !jj[t].include?(:javascript)
       k << jj[t]
-      end 
+      end
     t+=1
   end
-  
+
   c.to_h.each do |i,v|
   v.merge!(:style => "oo")
-  end 
-  
+  end
+
   k.to_h.each do |i,v|
   v.merge!(:style => "functional")
-  end 
-  
+  end
+
   b.each do |i,v|
   v.merge!(:style =>["functional", "oo"])
   end
-  
+
   b
-  
-  
+
+
   c+k+b
 
 
 
 
-  
+
 end
